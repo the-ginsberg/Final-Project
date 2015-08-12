5a65332f601803fccda093d800ea083d1c234014
Jeff Ginsberg <jeffginsberg87@gmail.com>
2015-08-11 22:55:19 -0400

    logout button working

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   resource :users
   root 'user#home'
+
+  devise_scope :user do get "/sign_out" => "devise/sessions#destroy"
+  # get '/sign_out' => 'devise/sessions#destroy'
+end
 end
