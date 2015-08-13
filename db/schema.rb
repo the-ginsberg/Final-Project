cdb0cc11091a58c5025750728f6e06e17e3f4470
Jeff Ginsberg <jeffginsberg87@gmail.com>
2015-08-12 22:34:54 -0400

    switched to postgres database

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20150813020153) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "dashboards", force: :cascade do |t|
     t.string   "title"
     t.string   "calendar"
@@ -21,7 +24,7 @@ ActiveRecord::Schema.define(version: 20150813020153) do
     t.datetime "updated_at", null: false
   end
 
-  add_index "dashboards", ["user_id"], name: "index_dashboards_on_user_id"
+  add_index "dashboards", ["user_id"], name: "index_dashboards_on_user_id", using: :btree
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
@@ -38,7 +41,8 @@ ActiveRecord::Schema.define(version: 20150813020153) do
     t.datetime "updated_at",                          null: false
   end
 
-  add_index "users", ["email"], name: "index_users_on_email", unique: true
-  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
+  add_foreign_key "dashboards", "users"
 end
