992ccfcd4066ee9e467058a21be1f6620e26a1a3
Jeff Ginsberg <jeffginsberg87@gmail.com>
2015-08-16 14:40:44 -0400

    signup and login redirect complete

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,6 +6,15 @@ class ApplicationController < ActionController::Base
 
   protected
 
+  def after_sign_up_path_for(resource)
+    '/dashboards' # Or :prefix_to_your_route
+  end
+
+  def after_sign_in_path_for(resource)
+    '/dashboards' # Or :prefix_to_your_route
+  end
+
+
   def configure_permitted_parameters
     devise_parameter_sanitizer.for(:sign_up) << :name;
     devise_parameter_sanitizer.for(:sign_up) << :phone_number;
