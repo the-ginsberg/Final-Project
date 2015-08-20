5f3f71b7e774ff9299acf662dbd69560231447ff
Jeff Ginsberg <jeffginsberg87@gmail.com>
2015-08-19 21:13:22 -0400

    Steve Whirly Fixes

--- app/models/dashboard.rb
+++ app/models/dashboard.rb
@@ -5,6 +5,7 @@ class Dashboard < ActiveRecord::Base
   has_many :news_feeds, dependent: :destroy
 
   validates :user_id, presence: true
+  validates :title, uniqueness: true
 
 
   def set_user(user)
