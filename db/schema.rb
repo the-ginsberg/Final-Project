29934e5472d682f415b6fcb4f79629022b6cb34e
Jeff Ginsberg <jeffginsberg87@gmail.com>
2015-08-21 12:50:02 -0400

    created dashboard membership model

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150821130348) do
+ActiveRecord::Schema.define(version: 20150821140342) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "dashboard_memberships", force: :cascade do |t|
+    t.integer  "member_id"
+    t.integer  "dashboard_id"
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
+  end
 
   create_table "dashboards", force: :cascade do |t|
     t.string   "title"
