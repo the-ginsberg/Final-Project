ff62135fcf61d145c03eba0a66e05d059a318aa3
Jeff Ginsberg <jeffginsberg87@gmail.com>
2015-08-22 16:52:05 -0400

    small fixes

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -13,17 +13,11 @@ class DashboardsController < ApplicationController
     @dashboard = Dashboard.find_by(token: params[:search])
     @news_feed = NewsFeed.new
     if @dashboard != nil
-      @dashboard.dashboard_memberships.each do |y|
-        if y.member_id == current_user.id && y.dashboard_id == @dashboard.id
-          puts "already a member of this Dashboard"
-        else
           join = DashboardMembership.create
           join.member_id = current_user.id
           join.dashboard_id = @dashboard.id
           join.save
-        end
-      end
-      render 'show'
+          render 'show'
     else
       redirect_to dashboards_path
     end
