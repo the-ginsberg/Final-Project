055c173bbc3fbeb3a3c8a8053f5e79d49fbd7fed
Mashon Thomas <mashonthomas@yahoo.com>
2015-08-23 15:24:07 -0400

    troubleshoot uploads not working

--- app/controllers/uploads_controller.rb
+++ app/controllers/uploads_controller.rb
@@ -4,7 +4,7 @@ class UploadsController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    @uploads = @dashboard.upload.all.order(created_at: :asc).paginate(page: params[:page], per_page: 9)
+    @uploads = @dashboard.uploads.all.order(created_at: :asc).paginate(page: params[:page], per_page: 9)
   end
 
   def create
@@ -16,7 +16,8 @@ class UploadsController < ApplicationController
       file: params[:file],
       acl: :public_read
     )
-    dashboard = Dashboard.find((params["dashboard_id"].first.first.first(-1).last(+1).to_i))
+    dashboard = Dashboard.find(params["dashboard_id"])
+    # params[:dashboard_id].first.first.first(-1).last(-1).last(2).to_i
     # Create an object for the upload
     @upload = dashboard.uploads.new(
                           url: obj.public_url,
@@ -25,7 +26,7 @@ class UploadsController < ApplicationController
 
     # Save the upload
     if @upload.save
-      redirect_to dashboard_path(dashboard.id), success: 'File successfully uploaded'
+      redirect_to dashboard_uploads_path(dashboard.id), success: 'File successfully uploaded'
     else
       flash.now[:notice] = 'There was an error'
       render :new
